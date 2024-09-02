f9f9718434d64afc6b404388dfd5b5d8dc4a64f8
kumamotone <kumamotone@users.noreply.github.com>
2024-09-02 22:40:04 +0900

    message

--- ios/Classes/ChannelTalkFlutterPlugin.swift
+++ ios/Classes/ChannelTalkFlutterPlugin.swift
@@ -188,7 +188,7 @@ public class ChannelTalkFlutterPlugin: NSObject, FlutterPlugin {
     let workflowId = argMaps["workflowId"] as? String
     let message = argMaps["message"] as? String
 
-    ChannelIO.openChat(with: workflowId)
+    ChannelIO.openChat(with: workflowId, message)
     result(true)
   }
 
